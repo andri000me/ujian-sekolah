c60782e75a89cea919cf06d40a5266a9b9569cf9
e-compfast <alamhafidz01@gmail.com>
2020-02-04 15:31:26 +0700

    merubah login tanpa verifikasi e-mail

--- ujian-sekolah/public/script/js-login/login.js
+++ ujian-sekolah/public/script/js-login/login.js
@@ -124,3 +124,78 @@
 //     });
 //   }
 // }())
+
+
+
+
+
+
+
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAedQNccvGAzUrxSg0NSP-r_cgxG4TfOLs",
+    authDomain: "ujian-sekolah.firebaseapp.com",
+    databaseURL: "https://ujian-sekolah.firebaseio.com",
+    projectId: "ujian-sekolah",
+    storageBucket: "ujian-sekolah.appspot.com",
+    messagingSenderId: "904992602631",
+    appId: "1:904992602631:web:de75996276079a011908a2",
+    measurementId: "G-F3T5PJ9W60"
+  };
+  firebase.initializeApp(config);
+  const auth = firebase.auth();
+
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+
+//     window.location.href = "/ujian/index.html";
+ 
+//     var user = firebase.auth().currentUser;
+
+//     if(user != null){
+
+//       var email_id = user.email;
+//       document.getElementById("user_para").innerHTML = " Status Login : " + email_id;
+       
+//     }
+
+//   } else {
+//     // No user is signed in.
+//     window.location.href = "/";
+//   }
+// });
+
+  auth.onAuthStateChanged(function(cek_user) {
+    if (cek_user) {
+      var user = firebase.auth().currentUser;
+
+        window.location.href = "/ujian/index.html";
+      
+    } else {
+      console.log("Status belum diverifikasi");
+    }
+  });
+
+  $( "#btn-signin" ).click(function(e) {
+    e.preventDefault();
+    var email = $('#txtEmail-formLogin').val();
+    var password = $('#txtPassword-formLogin').val();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      $( ".field-login-password" ).addClass( "has-error" );
+      $( ".field-login-email" ).addClass( "has-error" );
+      $( ".field-login-email>.help-block" ).html( errorCode );
+    });
+  });
+
+// function logout(){
+//   firebase.auth().signOut();
+// }
+
+  $( "#btn-logout" ).click(function(e) {
+    logout();
+  });
