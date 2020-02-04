b9304f82a20bac6578cd108d5dde7e2b1cd40bc6
e-compfast <alamhafidz01@gmail.com>
2020-02-04 18:10:31 +0700

    menambah favicon dan script index js di folder public

---
+++ ujian-sekolah/public/ujian/script/js-login/index.js
@@ -0,0 +1,35 @@
+// Your web app's Firebase configuration
+  var config = {
+    apiKey: "AIzaSyAedQNccvGAzUrxSg0NSP-r_cgxG4TfOLs",
+    authDomain: "ujian-sekolah.firebaseapp.com",
+    databaseURL: "https://ujian-sekolah.firebaseio.com",
+    projectId: "ujian-sekolah",
+    storageBucket: "ujian-sekolah.appspot.com",
+    messagingSenderId: "904992602631",
+    appId: "1:904992602631:web:de75996276079a011908a2",
+    measurementId: "G-F3T5PJ9W60"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+    function cek_login(){
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        return true;
+      } else {
+          window.location.href = "/";
+        }
+    });
+  }
+  cek_login();
+
+  
+
+   function logout(){
+    firebase.auth().signOut().then(function() {
+      console.log("sign out berhasil");
+      window.location.href = "/";
+    }).catch(function(error) {
+      swal ( "Oops" ,  "Something went wrong!" ,  "error" )
+    });
+  }
